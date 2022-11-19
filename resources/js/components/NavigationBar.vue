12243927c8e7cb5314ae6f53cacdcd1c9ce048d3
Masashi Kawakami <dlold1962@gmail.com>
2022-11-20 00:00:38 +0900

    fix navigation bar / add textbooks

--- resources/js/components/NavigationBar.vue
+++ resources/js/components/NavigationBar.vue
@@ -56,7 +56,7 @@ export default {
           label: "Exams",
         },
         {
-          name: "TEXTBOOK_REGISTRATION",
+          name: "TEXTBOOK_LIST_VIEW",
           label: "Textbooks",
         },
       ];

0caf58907a73e476afac1aee37a1c3b03b0354c9
Masashi Kawakami <dlold1962@gmail.com>
2022-11-20 00:01:18 +0900

    add textbook list component

---
+++ resources/js/views/TextbookListView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <template></template>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data () {
+    return {};
+  },
+  created () {},
+  computed: {},
+  methods: {}
+}
+</script>

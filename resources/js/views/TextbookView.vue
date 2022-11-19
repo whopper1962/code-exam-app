adbeb96dad980b4d0959bdbd0f47021fc015b679
Masashi Kawakami <dlold1962@gmail.com>
2022-11-20 00:01:34 +0900

    add textbook details component

---
+++ resources/js/views/TextbookView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      textbookId: null
+    };
+  },
+  created () {
+    this.textbookId = this.$route.params.textbook_id;
+    console.error(this.textbookId);
+  },
+  computed: {},
+  methods: {}
+}
+</script>

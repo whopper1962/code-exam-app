093cb695d5ee05470cc251ba8d5d41ade316e838
Masashi Kawakami <dlold1962@gmail.com>
2022-11-23 12:57:06 +0900

    fix text details / get details from api

--- resources/js/views/TextbookView.vue
+++ resources/js/views/TextbookView.vue
@@ -1,19 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ textDetails }}</p>
+  </div>
 </template>
 <script>
 export default {
   components: {},
   data () {
     return {
-      textbookId: null
+      textbookId: null,
+      textDetails: {}
     };
   },
   created () {
     this.textbookId = this.$route.params.textbook_id;
-    console.error(this.textbookId);
+    this.getDetails();
   },
   computed: {},
-  methods: {}
+  methods: {
+    getDetails () {
+      axios.get(`/api/textbooks/${this.textbookId}`)
+      .then((response) => {
+        this.textDetails = response.data;
+        console.log(this.textDetails);
+      }).catch((error) => {
+        console.error(error);
+        this.$router.push({
+          name: 'TEXTBOOK_LIST_VIEW'
+        });
+      }).finally(() => {
+        this.isLoading = false;
+      });
+    }
+  }
 }
 </script>
